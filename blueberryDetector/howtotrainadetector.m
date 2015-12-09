42568f54e937162133888a75e11c8c7bd487ed63
Marcine Kelly <mkell008@fiu.edu>
2015-12-09 11:24:15 -0500

    how to train a detector and display results
    added how to export ROIs

--- blueberryDetector/howtotrainadetector.m
+++ blueberryDetector/howtotrainadetector.m
@@ -1,15 +1,17 @@
 %{
 How to train a detector
 First use the training image labeler app
+open detectBlueberry.mat
 select ROIs regions of interest
 save to a mat file
-export ROIs
+export ROIs as positiveInstances
 then continue with the code below
 %}
 
 %{
 imDir = fullfile('C:','Users','yourcomputername','Documents','MATLAB','Images');
 addpath(imDir);
+positiveInstances = matfile('detectBlueberry.mat');
 negativeFolder = fullfile('C:','Users','yourcomputername','Documents','MATLAB','nonBlueberries2');
 trainCascadeObjectDetector('blueberryDetector.xml',positiveInstances,negativeFolder,'FalseAlarmRate',0.2,'NumCascadeStages',5);
 %}
