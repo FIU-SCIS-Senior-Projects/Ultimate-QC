1c0c55cb3017d712c38dc72ae971b3dfa8534ea3
mkell008 <mkell008@fiu.edu>
2015-12-09 10:36:04 -0500

    how to train a detector and display results
    user story - recognize

---
+++ blueberryDetector/howtotrainadetector.m
@@ -0,0 +1,30 @@
+%{
+How to train a detector
+First use the training image labeler app
+select ROIs regions of interest
+save to a mat file
+export ROIs
+then continue with the code below
+%}
+
+%{
+imDir = fullfile('C:','Users','yourcomputername','Documents','MATLAB','Images');
+addpath(imDir);
+negativeFolder = fullfile('C:','Users','yourcomputername','Documents','MATLAB','nonBlueberries2');
+trainCascadeObjectDetector('blueberryDetector.xml',positiveInstances,negativeFolder,'FalseAlarmRate',0.2,'NumCascadeStages',5);
+%}
+
+[filename, user_canceled] = imgetfile;
+if user_canceled == 1
+    return
+end
+detector = vision.CascadeObjectDetector('blueberryDetector.xml');
+img = imread(filename);
+bbox = step(detector,img);
+detectedImg = insertShape(img,'rectangle',bbox, 'color','cyan');
+%or
+%detectedImg = insertShape(img,'rectangle',bbox);
+%or
+%detectedImg = insertObjectAnnotation(img,'rectangle',bbox,'berry');
+figure, imshow(detectedImg);
+%rmpath(imDir);
